49142c03b9c480a3e9effb82069522ce0cd0be7a
geyoga <yogadewantama@gmail.com>
2019-07-11 22:49:33 +0700

    Initial Commit

---
+++ VideoVersionTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoVersionTwo
+//
+//  Created by Georgius Yoga Dewantama on 11/07/19.
+//  Copyright © 2019 Georgius Yoga Dewantama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
